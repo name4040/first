25b9031bda6427ad3621d268ba9b43727642df07
name4040 <72868118+name4040@users.noreply.github.com>
2020-11-13 10:19:50 +0800

    Add files via upload

---
+++ fd/WebContent/fundmy.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- bt框架 -->
+<%@ include file="bt.jsp"%>
+<title>Insert title here</title>
+</head>
+<body>
+	<div class="container">
+	<%@include file="nav.jsp" %>
+		<ul class="nav nav-tabs">
+			<li class="nav-item"><a class="nav-link active" href="fundmy.jsp">我的基金</a></li>
+			<li class="nav-item"><a class="nav-link" href="fundkf.jsp">开放式基金净值</a></li>
+			<li class="nav-item"><a class="nav-link" href="fundfof.jsp">FOF</a></li>
+			<li class="nav-item"><a class="nav-link" href="fundfb.jsp">封闭式基金净值</a></li>
+			<li class="nav-item"><a class="nav-link" href="fundfj.jsp">分级基金净值</a></li>
+			<li class="nav-item"><a class="nav-link" href="fundhb.jsp">货币型基金净值</a></li>
+		</ul>
+	</div>
+</body>
+</html>
---
+++ fd/src/reg/LoginServlet.java
@@ -0,0 +1,70 @@
+package reg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); 
+		response.setCharacterEncoding("UTF-8");
+		
+		String code = request.getParameter("checkCode");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		//先比较校验码
+				//1.获取session中的校验码
+				HttpSession session=request.getSession();
+				String s_verf=(String) session.getAttribute("verf");
+				//2.比较输入的校验码与session里的校验码是否一致
+				if(s_verf.equals(code)) {
+					//3.如果一致的话，校验用户名和密码	
+					if("1".equals(username)&&"1".equals(password)) {
+						//获取会话对象
+						//HttpSession session=request.getSession();
+						session.setAttribute("username", username);
+						//移除session中的校验码属性	
+						session.removeAttribute("verf");
+						//正确,转发到成功页面
+						RequestDispatcher rd=request.getRequestDispatcher("index1.jsp");
+						rd.forward(request, response);
+					}
+					else {
+						//用户名密码不对
+						RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+						request.setAttribute("input_name", username);
+						request.setAttribute("errormsg","用户名密码错误！");
+						rd.forward(request, response);
+					}
+				}
+				else {
+					//输入校验码不对的话
+					RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+					request.setAttribute("input_name", username);
+					request.setAttribute("errormsg","校验码错误！");
+					rd.forward(request, response);
+				}			
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
